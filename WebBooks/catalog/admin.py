e9338af19e803c17230cef07f71b80e8b4599d88
noname52rus <matvik52rus@gmail.com>
2023-09-28 15:27:03 +0300

    first commit

---
+++ WebBooks/catalog/admin.py
@@ -0,0 +1,51 @@
+from django.contrib import admin
+from django.utils.html import format_html
+from .models import Book, BookInstance, Publisher, Author, Status, Genre, Language
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'photo', 'show_photo')
+    fields = ['last_name', 'first_name', ('date_of_birth', 'photo')]
+    readonly_fields = ['show_photo']
+
+    def show_photo(self, obj):
+        print(len({obj.photo.url}))
+        if len({obj.photo.url}) == 0:
+            return format_html(f'<img src = "/media/images/Default_Author.png" style = "max-height: 100px;">')
+        else:
+            return format_html(f'<img src = "{obj.photo.url}" style = "max-height: 100px;">')
+    show_photo.short_description = 'Фото'
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'genre', 'language', 'display_author', 'show_photo')
+    list_filter = ('genre', 'author')
+    inlines = [BooksInstanceInline]
+    readonly_fields = ['show_photo']
+
+    def show_photo(self, obj):
+        return format_html(f'<img src = "{obj.photo.url}" style = "max-height: 100px;">')
+    show_photo.short_description = 'Обложка'
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('book', 'status')
+    fieldsets = (
+        ('Экземпляр книги', {'fields': ('book', 'inv_nom')}),
+        ('Статус и окончание его действия', {'fields': ('status', 'due_back')})
+    )
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Publisher)
+admin.site.register(Status)
+admin.site.register(Genre)
+admin.site.register(Language)
+#admin.site.register(Book)
+#admin.site.register(BookInstance)
---
+++ WebBooks/catalog/models.py
@@ -0,0 +1,104 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=120, verbose_name='Имя автора', help_text='Введите Имя автора')
+    last_name = models.CharField(max_length=120, verbose_name='Фамилия автора', help_text='Введите Фамилию автора')
+    date_of_birth = models.DateField(help_text='Введите дату рождения', verbose_name='Дата рождения', null=True,
+                                     blank=True)
+    about = models.TextField(verbose_name='Сведения об авторе', help_text='Введите Сведения об авторе')
+    photo = models.ImageField(upload_to='images', help_text='Загрузите фото автора', verbose_name='Фото автора',
+                              null=True, blank=True)
+
+    def __str__(self):
+        return self.last_name
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=20, verbose_name='Издательство', help_text='Введите наименование издательства')
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=20, verbose_name='Язык книги', help_text='Введите язык книги')
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Жанр книги', help_text='Введите жанр книги')
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200,
+                             help_text='Введите название книги',
+                             verbose_name='Название книги')
+    genre = models.ForeignKey('Genre', on_delete=models.CASCADE,
+                              help_text='Выберите жанр книги',
+                              verbose_name='Жанр книги', null=True)
+    language = models.ForeignKey('Language', on_delete=models.CASCADE,
+                                 help_text='Выберите язык книги',
+                                 verbose_name='Язык книги', null=True)
+    publisher = models.ForeignKey('Publisher', on_delete=models.CASCADE,
+                                  help_text='Выберите издательство',
+                                  verbose_name='Издательство', null=True)
+    year = models.CharField(max_length=4,
+                            help_text='Введите год издания',
+                            verbose_name='Год издания')
+    author = models.ManyToManyField('Author',
+                                    help_text='Выберите автора книги',
+                                    verbose_name='Автор книги')
+    summary = models.TextField(max_length=1200,
+                               help_text='Введите краткое описание книги',
+                               verbose_name='Аннотация книги')
+    isbn = models.CharField(max_length=13,
+                            help_text='Должно содержать 13 символов',
+                            verbose_name='ISBN книги')
+    price = models.DecimalField(max_digits=7, decimal_places=2,
+                                help_text='Введите цену книги',
+                                verbose_name='Цена (руб.)')
+    photo = models.ImageField(upload_to='images',
+                              help_text='Введите изображение обложки',
+                              verbose_name='Изображение обложки')
+
+    def display_author(self):
+        return ', '.join([author.last_name for author in self.author.all()])
+    display_author.short_description = 'Авторы'
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.id)])
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=20, help_text='Введите статус экземпляра книги',
+                            verbose_name='Статус экземпляра книги')
+
+    def __str__(self):
+        return self.name
+
+
+# экземпляр книги
+class BookInstance(models.Model):
+    book = models.ForeignKey('Book', on_delete=models.CASCADE, null=True)
+    inv_nom = models.CharField(max_length=20, null=True,
+                               help_text='Введите инвентарный номер экземпляра', verbose_name='Инвентарный номер')
+    status = models.ForeignKey('Status', on_delete=models.CASCADE, null=True,
+                               help_text='Изменить состояние экземпляра', verbose_name='Статус экземпляра книги')
+    due_back = models.DateField(null=True, blank=True,
+                                help_text='Введите конец срока статуса', verbose_name='Дата окончания статуса')
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        return '%s %s %s' % (self.inv_nom, self.book, self.status)
