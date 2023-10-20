8f71fdf9168dad2b7657ea72d4cbc520e8a89573
noname52rus <matvik52rus@gmail.com>
2023-10-20 18:50:02 +0300

    добавлен ENV

--- WebBooks/WebBooks/settings.py
+++ WebBooks/WebBooks/settings.py
@@ -9,20 +9,25 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os.path
+import os, environ
 from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 import django.core.mail.backends.console
 
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Start Django-environ
+env = environ.Env()
+# reading .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hva1)b*byoovy5(v%slysbm1ui$f-&9i%_h*m=^6kx6t43c31#'
+# SECRET_KEY = 'django-insecure-hva1)b*byoovy5(v%slysbm1ui$f-&9i%_h*m=^6kx6t43c31#'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -137,4 +142,11 @@ LOGIN_REDIRECT_URL = '/'
 
 # настройка отправки email
 """ это пробная отправка на консоль """
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+'''это реальная отправка через smtp.gmail'''
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_SSL = True
+EMAIL_PORT = 465
+EMAIL_HOST_USER = "matvik52rus@gmail.com"
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
